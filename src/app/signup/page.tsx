a3a8675eab6ed006bec1f00f29e99a345e8c89e1
helga ariani A <helgaa99ariani@gmail.com>
2025-09-30 04:20:04 +0000

    buat validasi untuk password harus 8 karakter dan harus ada karakter sep

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -24,7 +24,9 @@ import { Separator } from '@/components/ui/separator';
 const formSchema = z.object({
   name: z.string().min(2, 'Name must be at least 2 characters.'),
   email: z.string().email('Invalid email address.'),
-  password: z.string().min(6, 'Password must be at least 6 characters.'),
+  password: z.string()
+    .min(8, 'Password must be at least 8 characters.')
+    .regex(new RegExp(".*[@#&.$].*"), 'Password must contain at least one special character: @, #, &, ., $'),
 });
 
 export default function SignUpPage() {
